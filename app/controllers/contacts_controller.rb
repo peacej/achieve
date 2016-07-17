6bac9586bc83f96cfafc1bfd3b3cc28bea2daa0a
Jerry Chi <jerrychi123@gmail.com>
2016-07-17 07:18:17 +0000

    first commit oh yeah!

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,27 @@
+class ContactsController < ApplicationController
+  def new
+   if params[:back]
+       @contact = Contact.new(contacts_params)
+   else
+       @contact=Contact.new
+#       binding.pry
+   end
+  end
+  def create
+    @contact=Contact.new(contacts_params)
+    if @contact.save
+      redirect_to root_path, notice: "お問い合わせありがとうございます！！（＾＾）"
+    else
+      render action: 'new'
+    end
+  end
+  def confirm   
+     @contact=Contact.new(contacts_params)
+     render :new if @contact.invalid?
+  end
+  
+  private
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+end
